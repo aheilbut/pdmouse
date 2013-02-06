8d3200c010e828a8b254eb1a4f17eee698da1191
Adrian Heilbut <heilbut@broadinstitute.org>
2013-02-06 13:47:16 -0500

    added counts and links to tables for subsets

--- py/web/pd_web.py
+++ py/web/pd_web.py
@@ -17,6 +17,7 @@ from mako.template import Template
 from mako.lookup import TemplateLookup
 
 import pd_analysis as pda
+import pd_locals
 import pandas
 
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
@@ -33,14 +34,15 @@ tl = TemplateLookup(directories=["templates"],
                     output_encoding="utf-8")
 
 # load pd data
-pd_all = pandas.read_table("/data/adrian/Dropbox/Projects/Broad/PD_mouse/results/Oct29/PD_arraydata.tab")
-pd_covar = pandas.read_table("/data/adrian/Dropbox/Projects/Broad/PD_mouse/results/Oct29/pd.covar.tab")
+pd_all = pandas.read_table(pd_locals.datadir + "/Oct29/PD_arraydata.tab")
+pd_covar = pandas.read_table(pd_locals.datadir + "/Oct29/pd.covar.tab")
 
 
-cp73_wide_info = cPickle.load(open("/home/adrian/Dropbox/Projects/Broad/PD_mouse/results/jan30/cp73_m.pandas.pickle"))
-cp101_wide_info = cPickle.load(open("/home/adrian/Dropbox/Projects/Broad/PD_mouse/results/jan30/cp101_m.pandas.pickle"))
+cp73_wide_info = cPickle.load(open(pd_locals.datadir + "/jan30/cp73_m.pandas.pickle"))
+cp101_wide_info = cPickle.load(open(pd_locals.datadir + "/jan30/cp101_m.pandas.pickle"))
 
-t2_up = cPickle.load(open("/home/adrian/Dropbox/Projects/Broad/PD_mouse/results/jan30/t2_up.pickle"))
+t2_up = cPickle.load(open(pd_locals.datadir + "/jan30/t2_up.pickle"))
+t2_down = cPickle.load(open(pd_locals.datadir + "/jan30/t2_down.pickle"))
 
 class PDC():
     def __init__(self):
@@ -49,7 +51,7 @@ class PDC():
 
     def index(self):
         t = tl.get_template("index.html")
-        return t.render()
+        return t.render(t2_up = t2_up, t2_down = t2_down)
 
     def probeset_report(self, probeset):
         t = tl.get_template("probeset_report.html")
@@ -83,17 +85,15 @@ class PDC():
         pass
 
 
-    def result_table(self, result_set, sort_field=0, sort_dir="DESC", rows=100, page=0):
+    def result_table(self, result_set, sort_field=0, sort_dir="DESC", rows=100, dimlist=None, page=0, cell_type=None, contrast=None):
         if result_set == "cp73":
             s = cp73_wide_info
         elif result_set == "cp101":
             s = cp101_wide_info
-    	elif result_set == "t2_up_cp73_low_vs_saline":
-            s = t2_up["CP73"]["low vs. saline"]		
-        elif result_set == "t2_up_cp73_high_vs_saline":
-            s = t2_up["CP73"]["high vs. saline"]     
-        elif result_set == "t2_up_cp73_low_vs_high":
-            s = t2_up["CP73"]["low vs. high"]     
+    	elif result_set == "t2_up":
+            s = t2_up[cell_type][contrast]		
+        elif result_set == "t2_down":
+            s = t2_down[cell_type][contrast]      
 
 
         if sort_dir == "ASC":
@@ -105,6 +105,8 @@ class PDC():
             s = s.sort([s.columns.values[int(sort_field)]], ascending=sort_asc)
         else:
             s = s
+
+        s["probe_img"] = ["<img width=250 src='/probeset/figure/%s/scatter/png'/>" % urllib.quote_plus(probe_id) for probe_id in s.probe_id]
 
         s["probe_id"] = ["<a href='/probeset/figure/%s/scatter/png'>%s</a>" % (urllib.quote_plus(probe_id), probe_id) for probe_id in s.probe_id]
         s["symbol"] = ["<a target='_ncbi' href='http://www.ncbi.nlm.nih.gov/gene/?term=%s'>%s</a>" % (sym, sym) for sym in s.symbol]
@@ -118,11 +120,27 @@ class PDC():
 
         t = tl.get_template("result_table.html")
 
-        return t.render_unicode(table = s.ix[s.index[row_start:row_start + 100],:],
+        if dimlist is None:
+            dimarray = range(0, len(s.columns)-1)
+            dimlist = ",".join([str(q) for q in dimarray])
+        else:
+            dimarray = [int(i) for i in dimlist.split(",")]
+            dimarray.sort()
+
+
+        if dimarray is not None:
+            column_list = s.columns[dimarray]
+        else:
+            column_list = s.columns
+
+        return t.render_unicode( fulltable = s.ix[s.index[row_start:row_start + 100],:],
+                                 displaytable = s.ix[s.index[row_start:row_start + 100], column_list],
                                  page=int(page), 
                                  col_widths={"gene_name" : "300px"},
                                  sort_field=int(sort_field),
                                  sort_dir=sort_dir,
+                                 dimlist=dimlist,
+                                 dimarray=dimarray,
                                  result_set=result_set)
 
 

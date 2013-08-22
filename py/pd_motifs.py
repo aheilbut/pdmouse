208e9071a3300345bcbab5a28c97616d58b5235e
Adrian Heilbut <heilbut@broadinstitute.org>
2013-08-21 23:17:27 -0400

    add initial motif enrichment analysis

---
+++ py/pd_motifs.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <codecell>
+
+import pandas as pd
+import urllib2
+import IPython
+
+# <codecell>
+
+mm9_refFlat = pd.read_table("/data/adrian/Dropbox/Data/mouse_genome/refFlat.txt", 
+                            names=["geneName", "name", "chrom", "strand", "txStart", 
+                                     "txEnd", "cdsStart", "cdsEnd", "exonCount", "exonStarts", "exonEnds"],
+                            sep="\t") 
+
+# <codecell>
+
+mm9_refFlat[0:10]["txEnd"] - mm9_refFlat[0:10]["txStart"]
+
+# <codecell>
+
+IPython.display.HTML( mm9_refFlat[0:10].to_html() )
+
+# <codecell>
+
+swissreg_sites = pd.read_table("/data/adrian/Dropbox/Data/swissregulon/mm9_sites.gff", sep="\t", skiprows=1,
+                               names=["chrom", "alg", "motif_type", 
+                                      "start", "end", "score", "strand", "frame", "data"])
+
+swissreg_sites["motif_name"] = swissreg_sites.apply( lambda x: urllib2.unquote( dict([a.split("=") for a in x["data"].split(";")])["Motif"] ), axis=1 )
+
+# <codecell>
+
+r = dict([a.split("=") for a in swissreg_sites.data[0].split(";")])["Motif"]
+
+# <codecell>
+
+r["Motif"].decode("utf-8")
+
+# <codecell>
+
+urllib2.unquote(r["Motif"])
+
+# <codecell>
+
+import IPython
+
+# <codecell>
+
+IPython.display.HTML( swissreg_sites[0:10].to_html() )
+
+# <codecell>
+
+swissreg_sites.to_csv("/data/adrian/Dropbox/Data/swissregulon/mm9_sites.tab", sep="\t", index=False)
+
+# <codecell>
+
+swissreg_sites[0:100].apply( lambda x: x["data"], axis=1) 
+
+# <codecell>
+
+mat_tf = []
+for l in open("/data/adrian/Dropbox/Data/swissregulon/mat_TF_associations.mm").readlines():
+    fields = l.split("\t")
+    motif = fields[0]
+    tfs = fields[2:]
+    for tf in tfs:
+        tfdata = tf.split(":")
+        mat_tf.append( {"motif_name" : motif,
+                        "tf_id_to_knownbind" : tfdata[0], 
+                        "tf_entrez_gene_id" : tfdata[1], 
+                        "tf_symbol" : tfdata[2],
+                        "tf_description" : tfdata[3] } )
+mat_tf = pd.DataFrame(mat_tf)
+
+# <codecell>
+
+mat_tf
+
+# <codecell>
+
+
---
+++ sql/swissreg_motifdb.sql
@@ -0,0 +1,83 @@
+CREATE TABLE mm9_refFlat (
+  geneName VARCHAR(255),
+  name VARCHAR(255),
+  chrom VARCHAR(255),
+  strand CHAR(1),
+  txStart BIGINT,
+  txEnd BIGINT,
+  cdsStart BIGINT,
+  cdsEnd BIGINT,
+  exonCount INTEGER,
+  exonStarts TEXT,
+  exonEnds TEXT
+);
+
+create index i_cdsStart on mm9_refFlat(cdsStart);
+create index i_cdsEnd on mm9_refFlat(cdsEnd);
+create index i_geneName on mm9_refFlat(geneName);
+create index i_chrom on mm9_refFlat(chrom);
+
+\copy mm9_refFlat from '/data/adrian/Dropbox/Data/mouse_genome/refFlat.txt' with delimiter as '   ' csv header;
+
+CREATE TABLE swissreg_sites (
+	chrom VARCHAR(255),
+ 	alg VARCHAR(255),
+  	motif_type VARCHAR(255),
+	motif_start BIGINT,
+	motif_end BIGINT, 
+ 	score FLOAT,
+	strand CHAR(1),
+	frame VARCHAR(5),
+ 	data VARCHAR(2000),
+	motif_name VARCHAR(50)
+)
+
+create index i_ss_chrom on swissreg_sites(chrom);
+create index i_ss_motifstart on swissreg_sites(motif_start);
+create index i_ss_motifname on swissreg_sites(motif_name);
+
+
+CREATE TABLE mm9_gene_motifs (
+	geneName VARCHAR(255),
+ 	chrom VARCHAR(255),
+	cdsStart BIGINT,
+	cdsEnd BIGINT,
+	gene_strand CHAR(1),
+	motif_type VARCHAR(255),
+	motif_score FLOAT,
+	motif_start BIGINT,
+	motif_end BIGINT,
+	motif_name VARCHAR(50)
+)
+
+create index i_mg_geneName on mm9_gene_motifs(geneName);
+create index i_mg_motif_name on mm9_gene_motifs(motif_name);
+
+
+select distinct geneName, motif_name from mm9_gene_motifs
+
+
+insert into mm9_gene_motifs
+
+select geneName, mm9_refFlat.chrom, cdsStart, cdsEnd, mm9_refFlat.strand, motif_type, score, 
+	motif_start, motif_end, motif_name
+ from mm9_refFlat 
+ INNER JOIN swissreg_sites
+  ON 
+  swissreg_sites.chrom = mm9_refFlat.chrom
+  AND
+  (
+  (mm9_refFlat.strand = '+' 
+  	AND
+   swissreg_sites.motif_start < mm9_refFlat.cdsStart
+  	AND
+   swissreg_sites.motif_start > (mm9_refFlat.cdsStart - 10000))
+  OR
+  (mm9_refFlat.strand = '-' 
+  	AND
+   swissreg_sites.motif_start > mm9_refFlat.cdsEnd
+  	AND
+   swissreg_sites.motif_start < (mm9_refFlat.cdsEnd + 10000))
+  )
+
+
